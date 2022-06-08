3fb782c82e66efc187cd7d01b5b8bff7ee9fb687
ASMAA <96378262+asmaaos@users.noreply.github.com>
2022-06-08 10:05:15 +0300

    Add files via upload

---
+++ exam/script.js
@@ -0,0 +1,12 @@
+var cookieDiv = document.querySelector(".cookie");
+
+
+function accept() {
+    cookieDiv.remove();
+}
+function normalImg() {
+    document.getElementById("iii").src = "assets/succulents-1.jpg"
+  }
+  function singing() {
+    document.getElementById("iii").src = "assets/succulents-2.jpg"
+  }
